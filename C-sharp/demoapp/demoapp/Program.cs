254576e93312106ad51e5eccccba6f931413aaa2
deepakrio <deepakrio64@gmail.com>
2022-04-13 15:17:56 +0530

    commit csharp

---
+++ C-sharp/demoapp/demoapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo
+{
+    public class demoapp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to C sharp programming");
+        }
+    }
+}
